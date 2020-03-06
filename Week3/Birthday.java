0d2619963015df931d567e21cfcdb185724de934
FatNat <natalie2743@hotmail.com>
2020-03-06 16:54:27 +0800

    update Week3

--- Week3/Birthday.java
+++ Week3/Birthday.java
@@ -1,5 +1,33 @@
 public class Birthday {
     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        int peopleSum = 1;
+        int[] count = new int[n];
 
+        for(int i = 0; i < trials; i++) {
+            boolean[] room = new boolean[n];
+            int people = 0;
+
+            while(true) {
+                // Choose a birthday for the next person, uniformly at random between 0 and n-1
+                int randomBirthday = (int) (Math.random() * n);
+                people++;
+                if(room[randomBirthday]) {
+                    count[people]++;
+                    break;
+                }
+                room[randomBirthday] = true;
+            }
+        }
+
+        int accumulatedCount = 0;
+        for(int j = 1; j < count.length; j++) {
+            accumulatedCount += count[j];
+            double probability = (double) accumulatedCount / trials;
+            System.out.printf("%-10d%-10d%-16s\n", j, count[j], probability);
+
+            if(probability >=0.5) break;
+        }
     }
 }
--- Week3/Minesweeper.java
+++ Week3/Minesweeper.java
@@ -1,5 +1,45 @@
+/******************************************************
+ * Minesweeper.java
+ *
+ * Deduce which cells contain hidden mines using
+ * clues about the number of mines in neighboring cells
+ * takes three integer command-line arguments m, n, and k
+ * prints an m-by-n grid of cells with k mines, using
+ * asterisks for mines and integers for the neighboring
+ * mine counts (with two space characters between each cell)
+ *
+ *
+ ******************************************************/
 public class Minesweeper {
     public static void main(String[] args) {
-        
+        int m = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+        int k = Integer.parseInt(args[2]);
+
+        // initialize grid
+        String[][] grid = new String[m][n];
+
+        // put mines
+        while(k > 0) {
+            int randomM = (int) (Math.random() * m);
+            int randomN = (int) (Math.random() * n);
+
+            if(grid[randomM][randomN] == null) {
+                grid[randomM][randomN] = "*";
+                k--;
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == null) {
+                    grid[i][j] = "0";
+                }
+                System.out.print(grid[i][j]+ "  ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
