63cc14b5b88ab4009c13fdec17ccb4278b871ebc
FatNat <natalie2743@hotmail.com>
2020-03-06 19:15:57 +0800

    Update Minesweeper.java

--- Week3/Minesweeper.java
+++ Week3/Minesweeper.java
@@ -17,29 +17,87 @@ public class Minesweeper {
         int k = Integer.parseInt(args[2]);
 
         // initialize grid
-        String[][] grid = new String[m][n];
+        int[][] grid = new int[m][n];
 
         // put mines
         while(k > 0) {
             int randomM = (int) (Math.random() * m);
             int randomN = (int) (Math.random() * n);
 
-            if(grid[randomM][randomN] == null) {
-                grid[randomM][randomN] = "*";
+            if(grid[randomM][randomN] != -1) {
+                grid[randomM][randomN] = -1;
                 k--;
             }
         }
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == null) {
-                    grid[i][j] = "0";
+                // count number of neighboring mine
+                // above
+                if(i > 0) { // second row and after
+                    if(grid[i][j] != -1 && grid[i - 1][j] == -1) {
+                        grid[i][j]++;
+                    }
+                    if(j > 0) {
+                        // upperleft
+                        if(grid[i][j] != -1 && grid[i - 1][j - 1] == -1) {
+                            grid[i][j]++;
+                        }
+                    }
+                    if(j < n - 1) {
+                        // upperright
+                        if(grid[i][j] != -1 && grid[i - 1][j + 1] == -1) {
+                            grid[i][j]++;
+                        }
+                    }
                 }
-                System.out.print(grid[i][j]+ "  ");
+
+                if(j > 0) { // second column and after
+                    // left
+                    if(grid[i][j] != -1 && grid[i][j - 1] == -1) {
+                        grid[i][j]++;
+                    }
+                    if(i < m - 1) {
+                        // lowerleft
+                        if(grid[i][j] != -1 && grid[i + 1][j - 1] == -1) {
+                            grid[i][j]++;
+                        }
+                    }
+                }
+
+                if(i < m - 1) {
+                    // below
+                    if(grid[i][j] != -1 && grid[i + 1][j] == -1) {
+                        grid[i][j]++;
+                    }
+                }
+
+                if(j < n - 1) {
+                    // right
+                    if(grid[i][j] != -1 && grid[i][j + 1] == -1) {
+                        grid[i][j]++;
+                    }
+                }
+
+                if(i < m - 1 && j < n - 1) {
+                    // lowerright
+                    if(grid[i][j] != -1 && grid[i + 1][j + 1] == -1) {
+                        grid[i][j]++;
+                    }
+                }
             }
             System.out.println();
         }
 
-
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == -1) {
+                    System.out.print("*  ");
+                } else {
+                    System.out.print(grid[i][j] + "  ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
